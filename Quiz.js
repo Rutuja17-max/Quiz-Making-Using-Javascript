0425dd630f27579edee44be028626fb4ef4da3ac
Rutuja17-max <63246414+Rutuja17-max@users.noreply.github.com>
2020-08-06 18:31:51 +0530

    Create Quiz.js

---
+++ Quiz.js
@@ -0,0 +1,35 @@
+function resfunc(){
+	var total=5;
+	var score=0;
+
+	var q1=document.forms["quiz"]["q1"].value;
+	var q2=document.forms["quiz"]["q2"].value;
+	var q3=document.forms["quiz"]["q3"].value;
+	var q4=document.forms["quiz"]["q4"].value;
+	var q5=document.forms["quiz"]["q5"].value;
+
+for(i = 1; i <= total; i++)
+	{
+		if(eval('q'+i) == null || eval('q'+i)=='')
+			{
+				alert('Your missed Question is ' +i);
+				return false;
+			}
+
+	}
+
+var answer=["b","a","d","b","d"];
+
+for(i = 1; i <= total;i++)
+	{
+		if(eval('q'+i) ==answer[i-1])
+			{
+				score++;
+			}
+	}
+
+var results = document.getElementById('result');
+	results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+	alert('You scored '+score+' out of ' +total);
+return false;
+}
